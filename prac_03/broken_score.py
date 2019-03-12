46e75f5c771e1624203b3a5dc61e34024a7a9f18
Chase_Hartley <chase.hartley@my.jcu.edu.au>
2019-03-13 00:51:14 +1000

    add check_score function and made it print from main

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,17 +3,24 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
+def main():
 
-if score > 100:
-    print("invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-elif score < 50 < 0:
-    print("Failed")
-else:
-    print("invalid score")
+    score = float(input("Enter score: "))
+    print(check_score(score))
+
+
+def check_score(score):
+    if score > 100:
+        return "invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score > 0 < 50:
+        return "Failed"
+    else:
+        return "invalid score"
+
+
+main()
