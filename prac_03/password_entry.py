056e9619f5b86e737a0942df899cbd913b21fbe2
Chase_Hartley <chase.hartley@my.jcu.edu.au>
2019-03-12 23:19:27 +1000

    refactor password program to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -2,7 +2,18 @@
 
 
 def main():
+    password = get_password()
+    obscures_password(password)
+
+
+def obscures_password(password):
     show_user = ""
+    for i in range(password_length_checker(password)):
+        show_user = "*" + show_user
+    print(show_user)
+
+
+def get_password():
     password = input("Please enter your password:"
                      "\npassword must be at least 6 characters long"
                      "\n>>> ")
@@ -10,9 +21,7 @@ def main():
         password = input("Incorrect password:"
                          "\npassword must be at least 6 characters long"
                          "\n>>> ")
-    for i in range(password_length_checker(password)):
-        show_user = "*" + show_user
-    print(show_user)
+    return password
 
 
 """Question: Why does 'i' say there is no 'local variable' when in a function but when in a blank program
