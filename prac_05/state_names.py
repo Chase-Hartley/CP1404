3e2c5fc0180222c0572cbefa3c662632635345c9
jc319780 <chase.hartley@my.jcu.edu.au>
2019-03-30 13:05:04 +1000

    Added loop to print all functions with a basic string formation.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,10 +9,13 @@ STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Te
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
-state = input("Enter short state: ")
-while state != "":
-    if state.upper() in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state.upper()])
-    else:
-        print("Invalid short state")
-    state = input("Enter short state: ")
+# state = input("Enter short state: ")
+# while state != "":
+#     if state.upper() in STATE_NAMES:
+#         print(state, "is", STATE_NAMES[state.upper()])
+#     else:
+#         print("Invalid short state")
+#     state = input("Enter short state: ")
+
+for shortened_state, state in STATE_NAMES.items():
+    print("{} is {}".format(shortened_state, state))
